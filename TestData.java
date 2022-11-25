3aa42962f2809c95391b3cd50b46f18a119a0b01
Jacob Reinikainen Lindström <80760206+jazzjacob@users.noreply.github.com>
2022-11-25 16:33:54 +0100

    user input made

---
+++ TestData.java
@@ -0,0 +1,28 @@
+// Jacob Reinikainen Lindström, jare2673
+
+/**
+ * Denna klass är bara till för att möjliggöra testning av de klasser och
+ * metoder som du själv får namnge. Många av JUnit-testfallen till
+ * inlämingsuppgiftens olika delar behöver kunna identifiera dessa, och de
+ * kommer att leta efter namnen här.
+ * 
+ * De konstanter som finns nedan är de som behövs för testen till U6.3,
+ * inläsningsklassen och . Du kommer själv att behöva lägga till ytterligare
+ * konstanter i filen när du går in på uppgifterna till föreläsning 7 och
+ * framåt. Namnen på dessa kommer att stå i instruktionerna för uppgifterna, och
+ * kan också ses i JUnit-testfallen för dem.
+ */
+public final class TestData {
+
+	// Dessa fyra namn måste vara korrekt satta för att kunna testa alla uppgifter
+	// som ska läsa in data från användaren, till exempel U6.3
+
+	public static final String SCANNER_ADAPTER_CLASS_NAME = "UserInput";
+	public static final String READ_TEXT_METHOD_NAME = "string";
+	public static final String READ_INTEGER_METHOD_NAME = "integer";
+	public static final String READ_DECIMAL_METHOD_NAME = "decimal";
+
+	// Här lägger du in ytterligare konstanter när uppgiftsinstruktionerna ber om
+	// det.
+
+}
---
+++ UserInput.java
@@ -0,0 +1,42 @@
+// Jacob Reinikainen Lindström, jare2673
+
+import java.io.InputStream;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class UserInput {
+	
+	private static ArrayList<InputStream> list = new ArrayList<>();
+	private Scanner input;
+	
+	public UserInput(InputStream inputStream) {
+		if (list.contains(inputStream)) {
+			throw new IllegalStateException("Input");
+		}
+		list.add(inputStream);
+		input = new Scanner(inputStream);
+	}
+	
+	public UserInput() {
+		this(System.in);
+	}
+	
+	public int integer(String userPrompt) {
+		System.out.print(userPrompt + "?>");
+		int userInt = input.nextInt();
+		input.nextLine();
+		return userInt;
+	}
+	
+	public double decimal(String userPrompt) {
+		System.out.print(userPrompt + "?>");
+		double userDouble = input.nextDouble();
+		input.nextLine();
+		return userDouble;
+	}
+	
+	public String string(String userPrompt) {
+		System.out.print(userPrompt + "?>");
+		return input.nextLine();
+	}
+} 
